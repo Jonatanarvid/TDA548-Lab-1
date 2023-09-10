d0eda37e0d7322810d8b8fa14d128138b0fae891
Jonatanarvid <jonatan2004@hotmail.se>
2023-09-10 23:15:16 +0200

    Init samt början på tokenize funktion

---
+++ wordfreq.py
@@ -0,0 +1,11 @@
+def tokenize(lines: list) -> list:
+    words = []
+    for line in lines:
+        start = 0 
+        while start < len(line):
+            while line[start].isspace(): # Skipping blankspace characters
+                start += 1
+            print(line[start])
+            start += 1
+    return words
+tokenize(['    sweet  apple  tart'])
